44b4a39486d63809967779fee2889f1ac854cec0
Yushan Xu <jgu23ryu@uea.ac.uk>
2023-12-25 20:51:02 +0000

    gameplay done, delete account and character features done

--- character.py
+++ character.py
@@ -4,17 +4,22 @@ class Character:
         self.hair_length = hair_length
         self.hair_color = hair_color
         self.eye_color = eye_color
+        self.bonus = 0
+
 
     def view_character(self):
         return (f"Name: {self.name}, "
                 f"Hair Length: {self.hair_length}, "
                 f"Hair Color: {self.hair_color},"
-                f"Eye Color: {self.eye_color}")
+                f"Eye Color: {self.eye_color},"
+                f"bonus: {self.bonus}")
+
 
     def to_dict(self):
         return {
             'name': self.name,
             'hair_length': self.hair_length,
             'hair_color': self.hair_color,
-            'eye_color': self.eye_color
+            'eye_color': self.eye_color,
+            'bonus': self.bonus
         }
